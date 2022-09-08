8a2aed9ede3a8f29535ad6f5121b967279177a13
leo <loongtop@gmail.com>
2022-09-08 17:05:27 -0400

    Snapshot 0.7 - adding advice folder - complete validation function

--- service/service_education/src/main/java/com/gkhy/eduservice/controller/TeacherController.java
+++ service/service_education/src/main/java/com/gkhy/eduservice/controller/TeacherController.java
@@ -6,28 +6,38 @@ import com.gkhy.eduservice.repository.TeacherRepository;
 import com.gkhy.eduservice.service.TeacherService;
 import com.gkhy.servicebase.controller.ControllerBase;
 import com.gkhy.servicebase.result.Result;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Min;
+
 /**
- * <p>
- * teacher controller
- * </p>
- *
- * @author leo
- * @since 2022-07-11
- */
+ * @Name: TeacherController
+ * @Description:
+ * @Author: leo
+ * @Created: 2022-09-08
+ * @Updated: 2022-09-08
+ * @Version: 1.0
+ **/
 @RestController
 @CrossOrigin
 @RequestMapping("/eduservice/teacher")
 public final class TeacherController {
 
     private final TeacherService teacherService;
-    @Autowired
     public TeacherController(TeacherService teacherService) {
         this.teacherService = teacherService;
     }
+
+    //Method of conditional query with pagination
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public Result pageTeacherCondition(@PathVariable @Min(1) int current, @PathVariable @Min(1) int limit,
+                                       @RequestBody(required = false) TeacherVo teacherQuery) {
+
+        Page<TeacherEntity> eduTeacherList = teacherService.findAll(teacherQuery, current, limit);
+
+        long total = eduTeacherList.getNumberOfElements ();
+
+        return Result.success().data("total",total).data("rows",eduTeacherList.getContent());
+   }
 }
-
-
