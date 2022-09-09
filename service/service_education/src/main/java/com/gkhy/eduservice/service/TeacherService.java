505cc6614bd8e225b01f6a7b3a93764a14d3a711
leo <loongtop@gmail.com>
2022-09-08 23:21:26 -0400

    Snapshot 0.71 - delete IControllerBase.java for using @Min in the file ControllerBase.java

--- service/service_education/src/main/java/com/gkhy/eduservice/service/TeacherService.java
+++ service/service_education/src/main/java/com/gkhy/eduservice/service/TeacherService.java
@@ -3,11 +3,8 @@ package com.gkhy.eduservice.service;
 import com.gkhy.eduservice.entity.TeacherEntity;
 import com.gkhy.eduservice.entity.vo.TeacherVo;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 
 public interface TeacherService {
+    Page<TeacherEntity> findAll(TeacherVo teacherQuery, int current, int limit);
 }
